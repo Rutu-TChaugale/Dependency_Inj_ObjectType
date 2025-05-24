66149098736ef21465c874dee37222410b6a3291
rutika <chughaleruti7770@gmail.com>
2025-05-24 10:49:08 +0530

    Added setter-based Dependency Injection example using object type in Spring

---
+++ Sixth_DependencyInjec_ObjectType_setterbase/src/com/sit/client/Test.java
@@ -0,0 +1,21 @@
+package com.sit.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sit.model.Student;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext ap=new ClassPathXmlApplicationContext("beans.xml");
+		
+		Student student = ap.getBean(Student.class);
+		System.out.println(student.getRollno());
+		System.out.println(student.getName());
+		System.out.println(student.getEmail());
+		System.out.println(student.getAddr().getCityName());
+		System.out.println(student.getAddr().getAreaName());
+	}
+
+}
---
+++ Sixth_DependencyInjec_ObjectType_setterbase/src/com/sit/model/Student.java
@@ -0,0 +1,36 @@
+package com.sit.model;
+
+public class Student {
+	
+	private int rollno;
+	private String name;
+	private String email;
+	private Address addr;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getAddr() {
+		return addr;
+	}
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+	
+	
+
+}
